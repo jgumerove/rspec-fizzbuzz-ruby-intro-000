731d0190df1c72e221c29c2626eba5740d1a39c2
Josh Gumerove <josh.gumerove@gmail.com>
2020-10-10 20:31:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
+
+#Don't forget! This file needs to be 'required' in its spec file
+def fizzBuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
+  end
+end
+
+ fizz_buzz(3)
+ fizz_buzz(5)
+ fizz_buzz(15)
+ fizz_buzz(90)
+ fizz_buzz(7)# See README.md for instructions on how to do this
